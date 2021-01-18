51294fce15026580cb7adf02eed74d13331fba9e
Ryan Zemlin <rzemlin@gmail.com>
2021-01-18 23:53:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if !position_taken?(board, index) && index.between? (0, 8)
+  if index.between? (0, 8)  && !position_taken?(board, index)
     return true
   end
 end
